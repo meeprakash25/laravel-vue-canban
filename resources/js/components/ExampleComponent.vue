aaeed0033278b453c58a6f2058518ebc8070bfbd
Prakash P <mee.prakash25@gmail.com>
2022-06-03 15:30:21 +0545

    initial commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="my-content">
+        asdasd
+        <h1>
+            heading
+        </h1>
+    </div>
+</template>
+
+import draggable from "vuedraggable"
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
+
+<style lang="scss">
+
+.my-content{
+    margin: 0 20px;
+    h1 {
+        text-align: right;
+        font-size: 30px!important;
+    }
+}
+</style>
